62517c6d172309a528cf94851490b2254ae8f5e3
Vimukthi Bandaragoda <vimukthi.nelanga@gmail.com>
2023-01-13 20:40:35 +0530

    navbar-updated

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,11 @@
+
 const NavBar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg shadow p-2 mb-5 bg-body-tertiary rounded">
-        <div className="container-lg">
+      <nav className="navbar navbar-expand-lg sticky-top shadow p-2 mb-5 px-5 bg-body-tertiary rounded">
+        <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            <img src="/favicon.png" alt="brand" />
+            <img src="/logo.png" alt="brand" />
           </a>
           <button
             className="navbar-toggler"
@@ -17,40 +18,63 @@ const NavBar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div
+            className="collapse navbar-collapse "
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-2 fw-bold text-uppercase">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
+                <a className="nav-link text-dark" href="#">
+                  home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+                <a className="nav-link text-dark" href="#">
+                  shop
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+                <a className="nav-link text-dark" href="#">
+                  promotion
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+                <a className="nav-link text-dark" href="#">
+                  blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-dark" href="#">
+                  about
+                </a>
+              </li>
+              <li className="nav-item me-3">
+                <a className="nav-link text-dark" href="#">
+                  contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-dark me-3 me-lg-0" href="#">
+                 <img src="/cart.svg" alt="" />
+                </a>
+              </li>
+              
+              <li className="nav-item">
+                <a className="nav-link me-3 me-lg-0" href="#">
+                <img src="/search.svg" alt="search-icon" />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link me-3 me-lg-0" href="#">
+                <img src="/heart.svg" alt="search-icon" />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link me-3 me-lg-0" href="#">
+                <img src="/user.svg" alt="search-icon" />
                 </a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 // import '../styles/globals.css'
 // import "bootstrap/dist/css/bootstrap.css"
-import "../css/main.min.css"
+import { useEffect } from "react";
+import "../css/main.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+  return <Component {...pageProps} />;
 }
